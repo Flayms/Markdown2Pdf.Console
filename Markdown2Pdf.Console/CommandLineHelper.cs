ddb32a0a1eac920b38a6d7e94b1977643da28fbf
Flayms <der.anton111@gmail.com>
2024-02-10 17:15:14 +0100

    refac: extra class for options parsing

---
+++ Markdown2Pdf.Console/CommandLineHelper.cs
@@ -0,0 +1,134 @@
+﻿using System.Reflection;
+using CommandLine;
+using CommandLine.Text;
+using Markdown2Pdf.Options;
+using PuppeteerSharp.Media;
+
+namespace Markdown2Pdf.Console;
+internal class CommandLineHelper(string[] args) {
+
+  private readonly string[] _args = args;
+  private ParserResult<Options>? _parserResult;
+
+  public bool TryCreateOptions(out Options cliOptions, out Markdown2PdfOptions options) {
+    options = null!;
+
+    var parser = new Parser(settings => {
+      settings.CaseInsensitiveEnumValues = true;
+      settings.CaseSensitive = false;
+      settings.HelpWriter = null;
+    });
+
+    var result = this._parserResult = parser.ParseArguments<Options>(this._args);
+    cliOptions = result.Value;
+
+    if (result.Tag == ParserResultType.NotParsed) {
+      _DisplayHelp(result, result.Errors);
+      return false;
+    }
+
+    if (!_TryCleanupCliOptions(cliOptions))
+      return false;
+
+    if (!_TryCreateMarkdown2PdfOptions(cliOptions, out options))
+      return false;
+
+    return true;
+  }
+
+  private static bool _TryCleanupCliOptions(Options options) {
+    if (options.InputPath == null)
+      return false;
+
+    options.OutputPath ??= Path.ChangeExtension(options.InputPath, "pdf");
+    // var currentDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "node_modules");
+
+    return true;
+  }
+
+  private bool _TryCreateMarkdown2PdfOptions(Options cliOptions, out Markdown2PdfOptions options) {
+    options = new Markdown2PdfOptions {
+      ModuleOptions = ModuleOptions.Remote,
+      // ModuleOptions = ModuleOptions.FromLocalPath(currentDir),
+      KeepHtml = cliOptions.KeepHtml
+    };
+
+    if (cliOptions.HeaderPath != null) {
+      if (!File.Exists(cliOptions.HeaderPath))
+        throw new Exception($"The file '{cliOptions.HeaderPath}' doesn't exist!");
+
+      options.HeaderHtml = File.ReadAllText(cliOptions.HeaderPath);
+    }
+
+    if (cliOptions.FooterPath != null) {
+      if (!File.Exists(cliOptions.FooterPath))
+        throw new Exception($"The file '{cliOptions.FooterPath}' doesn't exist!");
+
+      options.FooterHtml = File.ReadAllText(cliOptions.FooterPath);
+    }
+
+    var marginOptions = cliOptions.MarginOptions;
+    if (marginOptions != null) {
+      options.MarginOptions = new Markdown2Pdf.Options.MarginOptions {
+        Top = marginOptions.Top,
+        Bottom = marginOptions.Bottom,
+        Left = marginOptions.Left,
+        Right = marginOptions.Right
+      };
+    }
+
+    options.Theme = cliOptions.Theme.ToLower() switch {
+      "github" => Theme.Github,
+      "latex" => Theme.Latex,
+      "" => Theme.None,
+      _ => Theme.Custom(cliOptions.Theme),
+    };
+
+    if (!this._TryGetPropertyValue<CodeHighlightTheme>(cliOptions.CodeHighlightTheme, out var codeHighlightTheme))
+      return false;
+
+    options.CodeHighlightTheme = codeHighlightTheme;
+    options.DocumentTitle = cliOptions.DocumentTitle;
+    options.CustomHeadContent = cliOptions.CustomHeadContent;
+    options.IsLandscape = cliOptions.IsLandscape;
+
+    if (!this._TryGetPropertyValue<PaperFormat>(cliOptions.Format, out var paperFormat))
+      return false;
+
+    options.Format = paperFormat;
+    options.Scale = cliOptions.Scale;
+
+    if (cliOptions.TableOfContents != null) {
+      var isOrdered = cliOptions.TableOfContents == TableOfContentsType.Ordered;
+      options.TableOfContents = new TableOfContents(isOrdered, cliOptions.TableOfContentsMaxDepth);
+    }
+
+    return true;
+  }
+
+  private static void _DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errors) {
+    var helpText = HelpText.AutoBuild(result, h => {
+      _ = HelpText.DefaultParsingErrorsHandler(result, h);
+      h.AddEnumValuesToHelpText = true;
+      h.AddDashesToOption = true;
+      return h;
+    }, e => e, verbsIndex: true);
+
+    System.Console.WriteLine(helpText);
+  }
+
+  private bool _TryGetPropertyValue<T>(string propertyName, out T propertyValue) {
+    var property = typeof(T).GetProperty(propertyName,
+    BindingFlags.Static | BindingFlags.Public | BindingFlags.IgnoreCase);
+
+    if (property == null) {
+      _DisplayHelp(this._parserResult!, this._parserResult!.Errors);
+      propertyValue = default!;
+      return false;
+    }
+
+    propertyValue = (T)property.GetValue(null, null)!;
+    return true;
+  }
+
+}
--- Markdown2Pdf.Console/Program.cs
+++ Markdown2Pdf.Console/Program.cs
@@ -1,117 +1,21 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using CommandLine;
-using CommandLine.Text;
+﻿
+using System.Diagnostics;
 using Markdown2Pdf;
 using Markdown2Pdf.Console;
-using Markdown2Pdf.Options;
-using PuppeteerSharp.Media;
 
-var parser = new Parser(settings => {
-  settings.CaseInsensitiveEnumValues = true;
-  settings.CaseSensitive = false;
-  settings.HelpWriter = null;
-});
+var commandLineHelper = new CommandLineHelper(args);
 
-var result = parser.ParseArguments<Options>(args);
-var options = result.Value;
-
-if (result.Tag == ParserResultType.NotParsed) {
-  DisplayHelp(result, result.Errors);
+if (!commandLineHelper.TryCreateOptions(out var cliOptions, out var options)) {
   return;
 }
-
-if (options.InputPath == null)
-  return;
 
 Console.WriteLine("Converting markdown to pdf...");
 
-var outputPath = options.OutputPath ?? Path.ChangeExtension(options.InputPath, "pdf");
-var currentDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "node_modules");
+var converter = new Markdown2PdfConverter(options);
+await converter.Convert(Path.GetFullPath(cliOptions.InputPath!), Path.GetFullPath(cliOptions.OutputPath!));
 
-var markdown2PdfOptions = _CreateMarkdown2PdfOptions(options);
-var converter = new Markdown2PdfConverter(markdown2PdfOptions);
-await converter.Convert(Path.GetFullPath(options.InputPath), Path.GetFullPath(outputPath));
-
-Console.WriteLine($"Generated pdf at: {outputPath}");
+Console.WriteLine($"Generated pdf at: {cliOptions.OutputPath}");
 
 // TODO: what if started from different directory?
-if (options.OpenAfterConversion)
-  _ = Process.Start(new ProcessStartInfo { FileName = outputPath, UseShellExecute = true });
-
-static Markdown2PdfOptions _CreateMarkdown2PdfOptions(Options options) {
-  var markdown2PdfOptions = new Markdown2PdfOptions {
-    ModuleOptions = ModuleOptions.Remote,
-    // ModuleOptions = ModuleOptions.FromLocalPath(currentDir),
-    KeepHtml = options.KeepHtml
-  };
-
-  if (options.HeaderPath != null) {
-    if (!File.Exists(options.HeaderPath))
-      throw new Exception($"The file '{options.HeaderPath}' doesn't exist!");
-
-    markdown2PdfOptions.HeaderHtml = File.ReadAllText(options.HeaderPath);
-  }
-
-  if (options.FooterPath != null) {
-    if (!File.Exists(options.FooterPath))
-      throw new Exception($"The file '{options.FooterPath}' doesn't exist!");
-
-    markdown2PdfOptions.FooterHtml = File.ReadAllText(options.FooterPath);
-  }
-
-  var marginOptions = options.MarginOptions;
-  if (marginOptions != null) {
-    markdown2PdfOptions.MarginOptions = new Markdown2Pdf.Options.MarginOptions {
-      Top = marginOptions.Top,
-      Bottom = marginOptions.Bottom,
-      Left = marginOptions.Left,
-      Right = marginOptions.Right
-    };
-  }
-
-  markdown2PdfOptions.Theme = options.Theme.ToLower() switch {
-    "github" => Theme.Github,
-    "latex" => Theme.Latex,
-    "" => Theme.None,
-    _ => Theme.Custom(options.Theme),
-  };
-
-  markdown2PdfOptions.CodeHighlightTheme = GetPropertyValue<CodeHighlightTheme>(options.CodeHighlightTheme);
-
-  markdown2PdfOptions.DocumentTitle = options.DocumentTitle;
-  markdown2PdfOptions.CustomHeadContent = options.CustomHeadContent;
-  markdown2PdfOptions.IsLandscape = options.IsLandscape;
-  markdown2PdfOptions.Format = GetPropertyValue<PaperFormat>(options.Format);
-  markdown2PdfOptions.Scale = options.Scale;
-
-  if (options.TableOfContents != null) {
-    var isOrdered = options.TableOfContents == TableOfContentsType.Ordered;
-    markdown2PdfOptions.TableOfContents = new TableOfContents(isOrdered, options.TableOfContentsMaxDepth);
-  }
-
-  return markdown2PdfOptions;
-}
-
-static T GetPropertyValue<T>(string propertyName) {
-  var property = typeof(T).GetProperty(propertyName,
-  BindingFlags.Static | BindingFlags.Public | BindingFlags.IgnoreCase);
-
-  if (property == null) {
-    // TODO: dipslay help
-    throw new ArgumentException();
-  }
-
-  return (T)property.GetValue(null, null)!;
-}
-
-static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errors) {
-  var helpText = HelpText.AutoBuild(result, h => {
-    _ = HelpText.DefaultParsingErrorsHandler(result, h);
-    h.AddEnumValuesToHelpText = true;
-    h.AddDashesToOption = true;
-    return h;
-  }, e => e, verbsIndex: true);
-
-  Console.WriteLine(helpText);
-}
+if (cliOptions.OpenAfterConversion)
+  _ = Process.Start(new ProcessStartInfo { FileName = cliOptions.OutputPath, UseShellExecute = true });
